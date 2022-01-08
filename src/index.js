591622e364e9e821cb3d8f8c7be5f98dd315db49
mareedez <xxl_century@yahoo.com>
2022-01-07 21:13:42 -0800

    feat: final solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'
 ];
 
-const tenners = ['', 'ten', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '
+const tenners = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'
 ];
 
 
@@ -10,20 +10,27 @@ module.exports = function toReadable (number) {
     if (number < 100) {
         return tenner(number)
     } else {
-        return hundreds(number) + tenner(number % 100)
+        return hundreds(number)
     }
 }
 
 function tenner(value) {
     if (value < 20) {
         return numbers[value]
+    }
+    else if (value % 10 === 0) {
+        return tenners[value / 10]
     } else {
       let tenner = Math.floor(value/10)
-      return tenners[tenner] + numbers[value % 10]
+      return tenners[tenner] + ' ' + numbers[value % 10]
     }  
 }
 
 function hundreds(value) {
+    if (value % 100 === 0) {
+        return `${numbers[value / 100]} hundred`
+    } else {
     let hundreds = Math.floor(value/100)
-    return `${numbers[hundreds]} hundred `
+    return `${numbers[hundreds]} hundred ` + tenner(value % 100)
+    }
 }
